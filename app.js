acb267ba75c614c0c03d8251ebcdff67265ab662
maor <maorshaer87@gmail.com>
2022-12-12 15:10:16 +0200

    fourth attempt fail

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// giving classes and ids names
+    // giving classes and ids names
     let mainContainer = document.querySelector('.main-container');
     let resetButton = document.querySelector('.reset-button');
     let billing = document.querySelector('#bill');
-    let tipAmount = document.querySelector('.tip-num');
+    let tipAmount = document.querySelector('.pricing-num');
     let totalAmount = document.querySelector('.pricing-num2');
     let buttonPercentage = document.querySelector('.p-buttons');
     let fivePercent = document.querySelector('.five-percent');
@@ -14,22 +14,27 @@
     let numPeople = document.querySelector('.num-input');
 
 
-    class calculator {// naming the 3 sections
+    class Calculator {// naming the 3 sections
         constructor(amount, tip, persons) {
             this.amount = amount;
             this.tip = tip;
             this.persons = persons;
         }
-    }
-    // declaring the above constructor
-    let calculate = new calculator(0.00, 0.00, 0);
 
-   calculate = { // re naming to add in the above calculate
-    amount : billing.value,
-    tip : buttonPercentage.value,
-    persons : numPeople.value
-   }
-   
+    calculate() { // re naming to add in the above calculate
+        const tipAmount = this.amount * this.tip;
+        const totalAmount = this.amount + tipAmount;
+        return { tipAmount, totalAmount};
+     }
+
+    }  
+    // declaring the above constructor
+    calculate = new Calculator(0.00, 0.00, 0);
+
+    // update calculate to users input
+    calculate.amount = billing.value;
+    calculate.persons = numPeople.value;
+
    // convert all 3 to decimal numbers & persons to integer
    let amount = parseFloat(calculate.amount);
    let tip = parseFloat(calculate.tip);
@@ -76,42 +81,48 @@
         console.log(calCul);
     }
 
-    let divideFullPrice = function(){// making calculations to show result in greenContainer
-        amount.forEach( () => {
-            if(persons < tip){
-                return amount * tip / persons;
-            }
-        });
+    function handlePercentageButtonClick(event){// making calculations to show result in greenContainer
+       // calculate the tip & total amounts & update the corresponding element on page
+         event.calculate = tipAmount;
+         event.calculate = totalAmount;	
     }  
+
+    // Add event Listeners to the percentage buttons to 
+    // call the above function when clicked
+    fivePercent.addEventListener('click', handlePercentageButtonClick);
+    tenPercent.addEventListener('click', handlePercentageButtonClick);
+    fifteenPercent.addEventListener('click', handlePercentageButtonClick);
+    twentyfivePercent.addEventListener('click', handlePercentageButtonClick);
+    fiftyPercent.addEventListener('click', handlePercentageButtonClick);
     
     const re = /^\d{0,2}$/g; // rules for person input
 
     function reGex(e){// anything below 1 person will show invalid warning input
         const inValid = re.exec(e.value);
 
-        persons.textContent = inValid 
+       re = persons.textContent = inValid 
         ? `Can't be zero` : ``;
-    }
+    } 
 
     buttonPercentage.addEventListener('input', (event) => {
         event.preventDefault();
         reGex(persons);
-    });
+    }); 
 
     tipAmount = []; 
 
     totalAmount = [];
 
-    (function(element){ // connecting button percentages to display result in green container
+    (function disPlay(element){ // connecting button percentages to display result in green container
      if(element <= tip){
          tip.push(amount / persons);
 
          return tipAmount && totalAmount;
      }
-  })();
+  })(); 
      
 
-   function addCode(){// dynamically inserted by clicking reset
+   function addCode(){// dynamically inserted by clicking percentage button
        buttonPercentage = document.querySelector('.secondgreen-container').innerHTML += ` 
     
        <div class="tip-price">
@@ -123,8 +134,50 @@
        </div>
      `;
     }
+    
+    // additional functionality for percentage buttons
+    fivePercent.addEventListener('click', function(){
+        calculate;
+        addCode();
+    });
+
+    tenPercent.addEventListener('click', function(){
+        calculate;
+        addCode();
+    });
+
+    fifteenPercent.addEventListener('click', function(){
+        calculate;
+        addCode();
+    });
+
+    twentyfivePercent.addEventListener('click', function(){
+        calculate;
+        addCode();
+    });
+
+    fiftyPercent.addEventListener('click', function(){
+        calculate;
+        addCode();
+    });
+
+    // custom percentage is input
+    customInput.addEventListener('input', function(){
+        calculate;
+        addCode();
+    });
+
+    // update persons value to make people input change
+    numPeople.addEventListener('input', function(){
+        calculate;
+        addCode();
+    });
+     
+    
+    // defining function for percentage buttons 
+    buttonPercentage.addEventListener('click', addCode);
  
-    // reset button to delete all info
+    // click reset button to delete all info
     resetButton.addEventListener('click', function(data) {
         if(data <= calculate){
              return calculate.delete && tipAmount.delete && totalAmount.delete;
